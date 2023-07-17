ac670c88c0068df712461bc8807fbef3a54ac1a9
mattkotzbauer <mkotzbauer52@gmail.com>
2023-07-18 06:33:02 +0900

    thing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,92 +1,126 @@
-<!-- Unicorn.svelte -->
-
 <script>
-    // You can add any necessary logic here, but for simplicity, we'll skip it in this example.
-  </script>
-  
-  <style>
-    /* Basic styling */
-    .container {
-      max-width: 600px;
-      margin: 0 auto;
-      padding: 2rem;
-      text-align: center;
-    }
-  
-    .unicorn {
-      font-size: 3rem;
-      font-weight: bold;
-      margin-bottom: 1rem;
-      /* Use a gradient background for each letter */
-      background: linear-gradient(135deg, #ff4081, #ff8a65, #f9d423, #66bb6a, #18ffff, #aa00ff);
-      -webkit-background-clip: text;
-      background-clip: text;
-      color: transparent;
-    }
-  
-    .description {
-      font-size: 1.2rem;
-      margin-bottom: 2rem;
-      line-height: 1.6;
-      color: #444;
-    }
-  
-    /* Box switch styling */
-    .box-switch {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background-color: #eee;
-      border-radius: 25px;
-      padding: 0.5rem;
-    }
-  
-    .switch-label {
-      font-size: 1rem;
-      text-transform: uppercase;
-      color: #555;
-      font-weight: bold;
-    }
-  
-    .switch-box {
-      width: 50px;
-      height: 30px;
-      background-color: #fff;
-      border-radius: 15px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      cursor: pointer;
-      transition: all 0.3s ease;
-    }
-  
-    .switch-box.warning {
-      transform: translateX(0);
-      background-color: #ff9800; /* Warning color */
-    }
-  
-    .switch-box.block {
-      transform: translateX(100%);
-      background-color: #e91e63; /* Wallet block color */
-    }
-  </style>
-  
-  <div class="container">
-    <div class="unicorn">Unicorn</div>
-    <div class="description">
-      Unicorn is a Deep Learning-powered spending limiter that identifies historic losses and prevents you from repeating
-      them. Centered on the idea that predicting emotions is easier than foreseeing complex markets, Unicorn can be set to
-      either warn you or block your wallet when you reach a point of historic losses.
-    </div>
-    <div class="box-switch">
-      <div class="switch-label">Warning</div>
-      <div class="switch-box warning"> <!-- Add the "warning" class to move the square to the left -->
-        <!-- You can add an icon or any other visual indication inside this box if you want -->
-      </div>
-      <div class="switch-box block"> <!-- Add the "block" class to move the square to the right -->
-        <!-- You can add an icon or any other visual indication inside this box if you want -->
-      </div>
-      <div class="switch-label">Wallet Block</div>
-    </div>
-  </div>
-  
+	// State variables
+	let isWarningSelected = true;
+	let isWalletBlockSelected = false;
+
+	//Toggle selected options
+	function toggleWarning() {
+		isWarningSelected = true;
+		isWalletBlockSelected = false;
+	}
+
+	function toggleWalletBlock() {
+		isWarningSelected = false;
+		isWalletBlockSelected = true;
+	}
+</script>
+
+<div class="container">
+	<div class="unicorn">Unicorn</div>
+	<div class="description-container">
+		<!-- New container element -->
+		<div class="description">
+			Unicorn is a Deep Learning-powered spending limiter that identifies historic losses and
+			prevents you from repeating them. Centered on the idea that predicting emotions is easier than
+			foreseeing complex markets, Unicorn can be set to either warn you or block your wallet when
+			you reach a point of historic losses.
+		</div>
+	</div>
+	<div class="box-switch">
+		<div class="switch-label">Warning</div>
+		<div class="switch-box warning" class:selected={isWarningSelected} on:click={toggleWarning}>
+			<!-- You can add an icon or any other visual indication inside this box if you want -->
+		</div>
+		<div
+			class="switch-box block"
+			class:selected={isWalletBlockSelected}
+			on:click={toggleWalletBlock}
+		>
+			<!-- You can add an icon or any other visual indication inside this box if you want -->
+		</div>
+		<div class="switch-label">Wallet Block</div>
+	</div>
+</div>
+
+<style>
+	/* Basic styling */
+	.container {
+		max-width: 600px;
+		margin: 0 auto;
+		padding: 2rem;
+		text-align: center;
+	}
+
+	.unicorn {
+		font-size: 3rem;
+		font-weight: bold;
+		margin-bottom: 1rem;
+		/* Use a gradient background for each letter */
+		background: linear-gradient(135deg, #ff4081, #ff8a65, #f9d423, #66bb6a, #18ffff, #aa00ff);
+		-webkit-background-clip: text;
+		background-clip: text;
+		color: transparent;
+	}
+
+  .description {
+  font-size: 1.2rem;
+  margin-bottom: 1rem; /* Adjust the spacing between the text and the container's bottom line */
+  line-height: 1.6;
+  color: #444;
+}
+  .description-container {
+  padding: 1rem; /* Add some padding to create space between the text and the perimeter */
+  border: 1px solid #ccc; /* Thin grey perimeter */
+  border-radius: 5px; /* Rounded corners for the perimeter */
+  margin-bottom: 0.5rem; /* Decrease the vertical space between the description and the box-switch section */
+  margin-top: 1rem; /* Bring the description container up slightly */
+}
+
+	/* Box switch styling */
+	.box-switch {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: #eee;
+		border-radius: 25px;
+		padding: 0.5rem;
+		margin-bottom: 1rem; /* Increase vertical space between the description and the box-switch section */
+	}
+
+	.switch-label {
+		font-size: 1rem;
+		text-transform: uppercase;
+		color: #555;
+		font-weight: bold;
+	}
+
+	.switch-box {
+		width: 50px;
+		height: 30px;
+		background-color: #fff;
+		border-radius: 15px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		cursor: pointer;
+		transition: all 0.3s ease;
+	}
+
+	.switch-box.warning {
+		transform: translateX(0);
+		background-color: #000; /* Warning color */
+	}
+
+	.switch-box.block {
+		transform: translateX(100%);
+		background-color: #000; /* Wallet block color */
+	}
+
+	.switch-box.warning.selected {
+		background-color: #66bb6a; /* Green when selected */
+	}
+
+	.switch-box.block.selected {
+		background-color: #66bb6a; /* Black when selected */
+	}
+</style>
